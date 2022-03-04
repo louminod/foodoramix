b6643f981c320fb2c9e5cbf243d783d6b29fe2a8
Levasseur Sébastien <sebastien.levasseur@efrei.net>
2022-03-04 17:59:13 +0100

    Implemented new data structure and a display for all recipes

--- src/lib/typeorm.ts
+++ src/lib/typeorm.ts
@@ -12,8 +12,6 @@ import {
 import {Recipe} from "../entity/Recipe";
 import {Ingredient} from "../entity/Ingredient";
 import {Instruction} from "../entity/Instruction";
-import {RecipeIngredient} from "../entity/RecipeIngredient";
-import {RecipeInstruction} from "../entity/RecipeInstruction";
 import {User} from "../entity/User";
 import {Session} from "../entity/Session";
 
@@ -25,7 +23,7 @@ export function initConnection() {
         username: DATABASE_USER,
         password: DATABASE_PASS,
         database: DATABASE_NAME,
-        entities: [User, Session, Recipe, Ingredient, Instruction, RecipeIngredient, RecipeInstruction],
+        entities: [User, Session, Recipe, Ingredient, Instruction],
         synchronize: DATABASE_SYNC,
         logging: DATABASE_LOGGING,
         multipleStatements: true
